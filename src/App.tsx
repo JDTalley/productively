c303aad46b5b28f0696ab5d4a46cf0c6e92112c9
Dakota Talley <dakota.talley@timmons.com>
2022-08-06 16:19:41 -0400

    Remove legacy code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Pomodoro from './components/Pomodoro/Pomodoro'
-//import PomodoroTimer from './components/PomodoroTimer'
-//import Timer from './components/Pomodoro/Timer'
 import TaskList from './components/tasks/TaskList';
 import './App.css';
 
